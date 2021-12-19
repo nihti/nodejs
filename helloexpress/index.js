eb231e380b6aafab429530c3aa44caa5ef227488
nihti <22195470+nihti@users.noreply.github.com>
2021-12-19 14:19:52 +0200

    express folder

---
+++ helloexpress/index.js
@@ -0,0 +1,10 @@
+// https://expressjs.com/ 
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
--- helloworld/index.js
+++ helloworld/index.js
@@ -1,13 +1,30 @@
-const http = require('http');
+/** 
+ * CommonJS moduulijärjestelmä tuo http-moduulin
+ * Kurssilla käytetään CommonJS moduuleja
+ * ES6 import ja export myös mahdollisia avainsanoja
+ */
+ const http = require('http');
 
 const port = 3000;
+// ei tarpeen määritellä jos localhost
+const host = 'localhost';
 
+/** 
+ * createServer metodi 
+ * request ja response
+ * setHeader vastauksen otsikko  
+ * */ 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, () => {
+/**
+ * Käynnistää palvelimen ja kuuntelee porttia 3000 
+ * Muutoksien jälkeen käynnistettävä uudestaan ellei määritellä toisin 
+ * Host oletuksena localhost, mutta määriteltiin ylhäällä huvikseen
+ */
+server.listen(port, host, () => {
   console.log('Server is running at port ' + port);
 });
