75fd73f894158dfecaa3ae06925c19ba086cdf3f
nihti <22195470+nihti@users.noreply.github.com>
2021-12-19 19:50:11 +0200

    nodemovies added

--- helloexpress/index.js
+++ helloexpress/index.js
@@ -1,10 +1,40 @@
 // https://expressjs.com/ 
+// CommonJS moduulijärjestelmä tuo express-moduulin
 const express = require('express');
 const app = express();
 const port = 3000;
+// kuuntelee get-pyyntöjä, myös
+// .post, .put, .delete jne. 
+// .all kuuntelee kaikkia 
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  res.send("Hello Express! Hello nodemon! Test. heti");
+})
+// Parametrien käsittely
+app.get("/home/:fn/:ln", (req, res) => {
+  res.send(`Welcome to our page ${req.params.fn} ${req.params.ln}`);
+})
+// JSON response
+app.get("/home/user/", (req, res) => {
+  res.json({username: 'John'})
+})
+// if else 
+app.get("/home/user/:name/:age", (req, res) => {
+  if (req.params.age >= 18) {
+    res.send(`Welcome ${req.params.name}, you're ${req.params.ln} years old`);
+  } else {
+    res.send(`Hello ${req.params.name}, you're too young`);
+  }
+})
+
+// res.status(404).end(); asettaisi vain tilan
+// res.sendStatus(404); lähettää myös vastauksen 
+app.get("/*", (req, res) => {
+  res.sendStatus(404);
+})
+app.get("/about", (req, res) => {
+  res.send("About us...");
 })
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
+
---
+++ nodemovies/index.js
@@ -0,0 +1,13 @@
+/**
+ * HTTP-requestilla on 
+ * päätepiste tai reitti (endpoint/route) 
+ * menetelmä (method), 
+ * otsikkotiedot (header) ja 
+ * sisältö (body)
+ */
+
+/**
+ * RESTillä on menetelmät
+ * GET, POST, DELETE ja PUT
+ * julkiset REST APIt lista https://github.com/public-apis/public-apis 
+ */
